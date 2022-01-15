63bbac9ecb48a995750ca47b86f549d34f149bbb
kalp12 <kalpmegh2000@gmail.com>
2022-01-15 18:00:52 +0530

    Initial commit

---
+++ card3.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="css/style.css">
+<script src="https://kit.fontawesome.com/51074f9484.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+<div class="w3-container">
+  &nbsp;
+
+  <div class="card">
+    <div class="lab">
+      <p style="font-size:large;
+  margin-top: 20px;">Lehoa</p><br>
+    </div>
+
+
+    <div class="right" style="margin-top: 20px;">
+      <i class="fas fa-coins"></i> 14623<br>
+      <p>Points</p>
+    </div>
+
+
+    <center>
+      <img src="img/palm.jpg" alt="Avatar" class="center">
+    </center>
+
+
+    <div class="container" style="padding-top: 60px;float:left;margin-left:50px">
+      <p >Name: <b>BetterSelfToday</b></p>
+      <p class="birth">Birthdate: 12/12/2010</p>
+    </div>
+  <div class="foot">
+  
+    <p>Powered by  <b>BetterSelf</b></p>
+ 
+  </div>
+  </div>&nbsp;
+<div class="yes">
+  <a href="save.php"><button class="circlular" style="background-color : #2785C4;"><i class="fa fa-save"></i></button></a>
+  
+
+  <a href="add.php"><button style="background-color : #2785C4;"><i class="fa fa-plus"></i></button></a>
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Card task 1</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+  <?php
+  include 'navbar.php';
+  ?>
+
+  <div class="container-fluid">
+    <!-- Introduction section -->
+    <div class="row intro py-1" style="background-color : #82E0AA;">
+      <div class="col-sm-12 col-md">
+        <div class="heading text-center my-5">
+          <h3>Welcome to</h3>
+          <h1>Cards</h1>
+        </div>
+      </div>
+    </div>
+
+
+    &nbsp;
+    <!-- Activity section -->
+    <div class="row activity text-center">
+      <div class="col-md act">
+        <img src="img/palm.jpg" class="img-fluid">
+        &nbsp;
+        <br>
+        <a href="card1.php"><button style="background-color : #2785C4;">Card 1</button></a>
+      </div>
+
+
+      <div class="col-md act">
+        <img src="img/palm.jpg" class="img-fluid">
+        &nbsp;
+        <br>
+        <a href="card2.php"><button style="background-color : #2785C4;">Card 2</button></a>
+      </div>
+      <div class="col-md act">
+        <img src="img/palm.jpg" class="img-fluid">
+        &nbsp;
+        <br>
+        <a href="card3.php"><button style="background-color : #2785C4;">Card 3</button></a>
+      </div>
+    </div>
+    &nbsp;
+  </div>
+  <footer class="text-center mt-5 py-2">
+    <p>&copy 2022. Made by <b>KALP MOTA</b></p>
+  </footer>
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+</html>
